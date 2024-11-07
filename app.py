540bda9ce212c74a25eb4bf9e38ed54357e002d3
neo2309 <arsalan.temrikar@gmail.com>
2024-11-07 13:22:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 st.button('Hit me')
-st.data_editor('Edit data', data)
+# st.data_editor('Edit data', data)
 st.checkbox('Check me out')
 st.radio('Pick one:', ['nose','ear'])
 st.selectbox('Select', [1,2,3])
